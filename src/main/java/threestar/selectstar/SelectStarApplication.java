1103197637785c847df40e88c5eae8f9ff2ce119
tlagusejr <starc13@naver.com>
2023-10-12 18:37:24 +0900

    first

---
+++ src/main/java/threestar/selectstar/SelectStarApplication.java
@@ -0,0 +1,13 @@
+package threestar.selectstar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectStarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SelectStarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/threestar/selectstar/SelectStarApplicationTests.java
@@ -0,0 +1,13 @@
+package threestar.selectstar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectStarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
