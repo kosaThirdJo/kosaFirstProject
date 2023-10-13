53d46e78be96dd97e99ede2dd23ea8013f38eca9
gossong <thdwldh99@gmail.com>
2023-10-13 17:29:42 +0900

    Create MeetingVO

---
+++ src/main/java/threestar/selectstar/domain/MeetingVO.java
@@ -0,0 +1,26 @@
+package threestar.selectstar.domain;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class MeetingVO {
+
+	private int meetingId;
+	private int userId;
+	private String title;
+	private int category;
+	private int status;
+	private LocalDateTime applicationDeadline;
+	private int views;
+	private int recruitmentCount;
+	private int applicationCount;
+	private String location;
+	private String description;
+	private LocalDateTime creationDate;
+}
