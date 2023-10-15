fbcf585823808cf4d423196dcf2db517aa581c71
syeej <syee2joe@gmail.com>
2023-10-15 10:15:08 +0900

    add profile update function

--- src/main/java/threestar/selectstar/dao/UserMapper.java
+++ src/main/java/threestar/selectstar/dao/UserMapper.java
@@ -3,6 +3,7 @@ package threestar.selectstar.dao;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 import threestar.selectstar.domain.UserDTO;
 import threestar.selectstar.domain.UserVO;
 
@@ -13,6 +14,10 @@ public interface UserMapper {
     @Select("select * from user")
     public List<UserVO> getAllUserList();
 
-    @Select("select nickname, profile_photo, about_me, profile_content from user where user_id = #{userId}")
+    @Select("select user_id userId, nickname, email, profile_photo, about_me, profile_content from user where user_id= #{userId}")
     public UserDTO getUserProfileInfo(int userId);
+
+    @Update("update user set about_me= #{about_me}, profile_content= #{profile_content} where user_id= #{userId}")
+    public boolean updateProfileInfo(UserDTO userDTO);
+
 }
