8788a84785d8e849ca311a848a434fd0188cdebb
syeej <syee2joe@gmail.com>
2023-10-16 09:36:36 +0900

    add myinfo html, css files

--- src/main/java/threestar/selectstar/controller/MyPageController.java
+++ src/main/java/threestar/selectstar/controller/MyPageController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -17,12 +18,16 @@ public class MyPageController {
     private static final Logger log = LoggerFactory.getLogger(MyPageController.class);
     @Autowired
     UserMapper userDAO;
+
+    //이력관리 조회
     @GetMapping("/profile")
     public ModelAndView getUserProfileInfo(){
         ModelAndView mav = new ModelAndView();
+        //session 수정 후 반영 예정
         UserDTO userDTO = userDAO.getUserProfileInfo(1);
         mav.addObject("userDTO", userDTO);
         mav.setViewName("mypage");
+        log.info("id 확인 : "+userDTO.getUserId());
         log.info(userDTO.getProfile_content());
         log.info(userDTO.getAbout_me());
         log.info(userDTO.getNickname());
@@ -30,6 +35,7 @@ public class MyPageController {
         return mav;
     }
 
+    //이력관리 수정
     @PostMapping("/updateprofile")
     public String updateUserProfileInfo(UserDTO userDTO){
         log.info("userid "+userDTO.getUserId());
@@ -38,5 +44,32 @@ public class MyPageController {
         boolean result = userDAO.updateProfileInfo(userDTO);
         log.info("result "+result);
         return "redirect:/mypage/profile";
+    }
+    
+    //개인정보수정 조회
+    @GetMapping("/myinfo")
+    public ModelAndView getUserInfo(){
+        ModelAndView mav = new ModelAndView();
+        //세션 수정 후 반영 예정
+        UserDTO userDTO = userDAO.getUserInfo(1);
+        mav.addObject("userDTO", userDTO);
+        mav.setViewName("myinfo");
+        log.info(""+userDTO);
+        return mav;
+    }
+
+    //개인정보수정
+    @PostMapping("/updateinfo")
+    public String updateUserInfo(UserDTO userDTO){
+        log.info("userId  : "+userDTO.getUserId());
+        log.info(userDTO.getPassword());
+        log.info(userDTO.getEmail());
+        log.info(userDTO.getNickname());
+        log.info(userDTO.getLocation1());
+        log.info("updateuserinfo"+userDTO);
+        //관심분야 추후 추가
+        boolean result = userDAO.updateUserInfo(userDTO);
+        log.info("result "+result);
+        return "redirect:/mypage/myinfo";
     }
 }
--- src/main/java/threestar/selectstar/dao/UserMapper.java
+++ src/main/java/threestar/selectstar/dao/UserMapper.java
@@ -1,6 +1,7 @@
 package threestar.selectstar.dao;
 
 
+import org.apache.catalina.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -33,5 +34,15 @@ public interface UserMapper {
     //마이페이지(이력관리) 수정
     @Update("update user set about_me= #{about_me}, profile_content= #{profile_content} where user_id= #{userId}")
     public boolean updateProfileInfo(UserDTO userDTO);
+
+    //개인정보수정화면 조회
+    @Select("select user_id userId, nickname, email, profile_photo, location1, location2, "
+            + "interest_language, interest_framework, interest_job from user where user_id= #{userId}")
+    public UserDTO getUserInfo(int userId);
+
+    //개인정보 수정
+    @Update("update user set password= #{password}, email= #{email}, nickname= #{nickname}, "
+            +"location1= #{location1}, location2= #{location2}, interest_language= #{interest_language} where user_id= #{userId}")
+    public boolean updateUserInfo(UserDTO userDTO);
 
 }
