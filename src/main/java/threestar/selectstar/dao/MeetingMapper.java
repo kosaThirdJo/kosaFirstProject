f1e4cf07c80e7829b93bd1b846af110ee77fce5b
gossong <thdwldh99@gmail.com>
2023-10-17 09:05:41 +0900

    main, search 페이지

--- src/main/java/threestar/selectstar/dao/MeetingMapper.java
+++ src/main/java/threestar/selectstar/dao/MeetingMapper.java
@@ -1,6 +1,7 @@
 package threestar.selectstar.dao;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import threestar.selectstar.domain.MeetingVO;
 
@@ -21,4 +22,16 @@ public interface MeetingMapper {
     // 댓글 추가 입력폼.. 필수 만약 댓글올렷으면 리다이렉트..
 
     // 신청 폼 <-- 차후?
+
+    // 메인 - 최신글 조회 (ORDER BY) (현재는 4개만 출력)
+    @Select("SELECT * FROM meeting ORDER BY creation_date DESC LIMIT 4")
+    List<MeetingVO> getLatestMeetings();
+
+    // 메인 - 인기글 조회 (RANK) : 최근 일주일간 올라온 글 중에서 조회수 높은 것 10개
+    @Select("SELECT * FROM meeting WHERE DATEDIFF(NOW(), creation_date) <= 7 ORDER BY views DESC LIMIT 10")
+    List<MeetingVO> getPopularMeetings();
+
+    // 검색 - 모임글 검색 (제목 일치)
+    @Select("SELECT * FROM meeting WHERE title LIKE CONCAT('%', #{searchWord}, '%')")
+    List<MeetingVO> searchMeetings(@Param("searchWord") String searchWord);
 }
