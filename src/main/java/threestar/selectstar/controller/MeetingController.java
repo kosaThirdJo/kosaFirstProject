987835be26c5f053a844e0f50cb3958658bfd332
tlagusejr <starc13@naver.com>
2023-10-23 15:04:07 +0900

    form 양식 기본값 수정

--- src/main/java/threestar/selectstar/controller/MeetingController.java
+++ src/main/java/threestar/selectstar/controller/MeetingController.java
@@ -2,6 +2,7 @@ package threestar.selectstar.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,45 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
+class InterestMap{
+    HashMap<String, String> frameworkMap;
+    HashMap<String, String> jobMap;
+    HashMap<String, String> langMap;
+    InterestMap() {
+         frameworkMap = new HashMap<>();
+        jobMap = new HashMap<>();
+        langMap = new HashMap<>();
+        langMap.put("1", "Java");
+        langMap.put("2", "Python");
+        langMap.put("3", "C#");
+        langMap.put("4", "C++");
+        langMap.put("5", "JavaScript");
+        langMap.put("6", "Ruby");
+        langMap.put("7", "Swift");
+        langMap.put("8", "TypeScript");
+        langMap.put("9", "PHP");
+        frameworkMap.put("1", "Spring");
+        frameworkMap.put("2", "React");
+        frameworkMap.put("3", "Angular");
+        frameworkMap.put("4", "Vue.js");
+        frameworkMap.put("5", "Express.js");
+        frameworkMap.put("6", "Django");
+        frameworkMap.put("7", "Ruby on Rails");
+        frameworkMap.put("8", "Flask");
+        frameworkMap.put("9", "Laravel");
+        jobMap.put("1", "프론트엔드");
+        jobMap.put("2", "백엔드");
+        jobMap.put("3", "풀스택");
+        jobMap.put("4", "모바일 앱 개발");
+        jobMap.put("5","게임 개발");
+        jobMap.put("6", "데이터베이스");
+        jobMap.put("7", "데브옵스");
+        jobMap.put("8", "디자이너");
+        jobMap.put("9", "기획자");
+    }
+}
+
+
 @Controller
 @RequestMapping(value = {"/meeting", "meeting?category={category}"})
 public class MeetingController {
@@ -25,6 +65,7 @@ public class MeetingController {
     final
     UserMapper userDao;
     final ApplyMapper applyDao;
+    final InterestMap interestMap = new InterestMap();
 
     public MeetingController(MeetingMapper meetingDao,CommentMapper commentDao, UserMapper userDao, ApplyMapper applyDao) {
         this.meetingDao = meetingDao;
@@ -73,7 +114,10 @@ public class MeetingController {
                 for (String splitEle:
                         SplitInterestLanguage) {
                     if (splitEle != null && !splitEle.equals("")) {
-                        interestListLangEle.add(splitEle);
+                        if (interestMap.langMap.containsKey(splitEle)) {
+                            interestListLangEle.add(splitEle);
+                        }
+
                     }
                 }
             }
@@ -82,7 +126,9 @@ public class MeetingController {
                 for (String splitEle:
                         SplitInterestFramework) {
                     if (splitEle != null &&!splitEle.equals("")) {
-                        interestListFrameEle.add(splitEle);
+                        if (interestMap.frameworkMap.containsKey(splitEle)) {
+                            interestListFrameEle.add(splitEle);
+                        }
                     }
                 }
             }
@@ -91,7 +137,9 @@ public class MeetingController {
                 for (String splitEle:
                         SplitInterestJob) {
                     if (splitEle != null &&!splitEle.equals("")) {
-                        interestListJobEle.add(splitEle);
+                        if (interestMap.jobMap.containsKey(splitEle)) {
+                            interestListJobEle.add(splitEle);
+                        }
                     }
                 }
             }
