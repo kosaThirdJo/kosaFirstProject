5069cb3f19645eef5a28cddeeed048fb96b73ede
tlagusejr <starc13@naver.com>
2023-10-26 21:13:52 +0900

    기타 버그 수정

--- src/main/java/threestar/selectstar/controller/MeetingController.java
+++ src/main/java/threestar/selectstar/controller/MeetingController.java
@@ -1,8 +1,6 @@
 package threestar.selectstar.controller;
-
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -11,62 +9,23 @@ import threestar.selectstar.dao.CommentMapper;
 import threestar.selectstar.dao.MeetingMapper;
 import threestar.selectstar.dao.UserMapper;
 import threestar.selectstar.domain.*;
+import threestar.selectstar.etc.InterestMap;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
-class InterestMap{
-    HashMap<String, String> frameworkMap;
-    HashMap<String, String> jobMap;
-    HashMap<String, String> langMap;
-    InterestMap() {
-         frameworkMap = new HashMap<>();
-        jobMap = new HashMap<>();
-        langMap = new HashMap<>();
-        langMap.put("1", "Java");
-        langMap.put("2", "Python");
-        langMap.put("3", "C#");
-        langMap.put("4", "C++");
-        langMap.put("5", "JavaScript");
-        langMap.put("6", "Ruby");
-        langMap.put("7", "Swift");
-        langMap.put("8", "TypeScript");
-        langMap.put("9", "PHP");
-        frameworkMap.put("1", "Spring");
-        frameworkMap.put("2", "React");
-        frameworkMap.put("3", "Angular");
-        frameworkMap.put("4", "Vue.js");
-        frameworkMap.put("5", "Express.js");
-        frameworkMap.put("6", "Django");
-        frameworkMap.put("7", "Ruby on Rails");
-        frameworkMap.put("8", "Flask");
-        frameworkMap.put("9", "Laravel");
-        jobMap.put("1", "프론트엔드");
-        jobMap.put("2", "백엔드");
-        jobMap.put("3", "풀스택");
-        jobMap.put("4", "모바일 앱 개발");
-        jobMap.put("5","게임 개발");
-        jobMap.put("6", "데이터베이스");
-        jobMap.put("7", "데브옵스");
-        jobMap.put("8", "디자이너");
-        jobMap.put("9", "기획자");
-    }
-}
 
 
+// 미팅 페이지 메인 컨트롤러
 @Controller
 @RequestMapping(value = {"/meeting", "meeting?category={category}"})
 public class MeetingController {
-    final
-    MeetingMapper meetingDao;
-    final
-    CommentMapper commentDao;
-    final
-    UserMapper userDao;
+    // 매퍼 생성자 방식 등록
+    final MeetingMapper meetingDao;
+    final CommentMapper commentDao;
+    final UserMapper userDao;
     final ApplyMapper applyDao;
-    final InterestMap interestMap = new InterestMap();
-
     public MeetingController(MeetingMapper meetingDao,CommentMapper commentDao, UserMapper userDao, ApplyMapper applyDao) {
         this.meetingDao = meetingDao;
         this.commentDao = commentDao;
@@ -74,6 +33,7 @@ public class MeetingController {
         this.applyDao = applyDao;
     }
 
+    final InterestMap interestMap = new InterestMap();
     // 메인페이지
     @GetMapping("")
     public String meetingMain(HttpSession session,@RequestParam(value = "page",required = false,defaultValue = "0") int page,Model model, @RequestParam(value = "category",required = false) Integer category, @RequestParam(value = "order",required = false,defaultValue = "creation_date")String order){
@@ -279,7 +239,7 @@ public class MeetingController {
     }
     // 게시글 수정 페이지
     @GetMapping("/fix/{id}")
-    public String fixArticle(Model model,
+    public String fixArticleForm(Model model,
                              HttpSession session,
                              @PathVariable("id") int meetingId){
         MeetingDTO meetingVO = meetingDao.getMeetingArticleById(meetingId);
